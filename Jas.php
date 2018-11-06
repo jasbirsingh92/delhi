ca157eea291c43b28b3fde2d583a60ba6fc2723a
Jasbir Singh <1568521@TCS.com>
2018-11-06 10:45:13 -0500

    first git

---
+++ Jas.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hi,This is opneshift</p>'; ?> 
+ <?php echo '<p>This is a Blue Green Deployment Strategy</p>'; ?> 
+ </body>
+</html>
